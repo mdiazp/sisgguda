c1dba928c93db5e80f3b0d3e5708419ece408479
Manuel Alejandro Diaz Perez <manueldiazp92@gmail.com>
2018-10-22 10:56:15 -0400

    .gitignore

--- sissguda.sql
+++
@@ -1,8 +0,0 @@
-drop table users;
-
-create table users(
-  id         serial primary key,
-  username   varchar(100) not null unique, 
-  isAdmin    boolean not null default false,
-  createdAt timestamp not null  
-);
